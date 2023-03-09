cf3c6e5a4f98df8ca49abac8cc1b6c9f4c1f7a69
sandeepreddygantla <reddysandeep0904@gmail.com>
2023-03-09 06:38:34 +0530

    updated

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS,cross_origin
 import requests
 from urllib.request import urlopen as uReq
 import logging
@@ -10,90 +10,65 @@ import re
 import csv
 
 
-# Get the base directory of this script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(filename=os.path.join(BASE_DIR, "scrapper.log") , level=logging.INFO)
 
-# Set up a logging file in the base directory
-logging.basicConfig(filename=os.path.join(BASE_DIR, "scrapper.log"), level=logging.INFO)
 
-# Create the Flask app
 app = Flask(__name__)
 
-# This function renders the homepage
-@app.route("/", methods=['GET'])
+print("Youtube Scrap")
+@app.route("/", methods = ['GET'])
 def homepage():
     return render_template("index.html")
 
-# This function handles POST and GET requests to the '/review' endpoint
-@app.route("/review", methods=['POST', 'GET'])
+@app.route("/review" , methods = ['POST' , 'GET'])
 def index():
-    # Handle POST requests
     if request.method == 'POST':
         try:
-            # Get the search query from the form input
-            search_query = request.form['content'].replace(" ", "")
-            
-            # Set the user agent to a fake one to avoid getting blocked by Google
+            query = request.form['content'].replace(" ","")
+            # fake user agent to avoid getting blocked by Google
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
-            # Fetch the search results page from YouTube
-            response = requests.get(f"https://www.youtube.com/@{search_query}/videos", headers=headers)
-            response_text = response.text
+            # fetch the search results page
+            response = requests.get(f"https://www.youtube.com/@{query}/videos", headers=headers)
+            res = response.text
 
-            # Extract relevant information from the HTML response using regex
-            video_ids = re.findall('"videoRenderer":{"videoId":".*?"', response_text)
-            thumbnails = re.findall('"thumbnail":{"thumbnails":\[{"url":".*?"', response_text)
-            titles = re.findall('"title":{"runs":\[{"text":".*?"', response_text)
-            views = re.findall('"shortViewCountText":{"accessibility":{"accessibilityData":{"label":".*?"', response_text)
-            published_times = re.findall('"publishedTimeText":{"simpleText":".*?"', response_text)
+            # Video
+            videoids = re.findall('"videoRenderer":{"videoId":".*?"', res)
+            # thumbnail
+            thumbnails = re.findall('"thumbnail":{"thumbnails":\[{"url":".*?"', res)
+            # Title
+            titles = re.findall('"title":{"runs":\[{"text":".*?"', res)
+            # Views
+            views = re.findall('"shortViewCountText":{"accessibility":{"accessibilityData":{"label":".*?"', res)
+            # Published Time
+            published_time = re.findall('"publishedTimeText":{"simpleText":".*?"', res)
             
-            # Create a report list to hold all of the relevant video data
-            report_list = []
-            
-            # Iterate through the video data and append it to the report list
+            report_list = [
+                ['S No', 'Video url', 'Thumbnail', 'Title', 'Views', 'Published Time']
+            ]
             for i in range(5):
-                video_url = 'https://www.youtube.com/watch?v=' + video_ids[i].split('"')[-2]
-                thumbnail_url = thumbnails[i].split('"')[-2]
-                title = titles[i].split('"')[-2]
-                view_count = views[i].split('"')[-2]
-                published_time = published_times[i].split('"')[-2]
-                
-                temp = {
-                    'S No': i+1,
-                    'Video url': video_url,
-                    'Thumbnail': thumbnail_url,
-                    'Title': title,
-                    'Views': view_count,
-                    'Published Time': published_time
-                }
+                temp = []
+                temp.append(i+1)
+                temp.append('https://www.youtube.com/watch?v=' + videoids[i].split('"')[-2])
+                temp.append(thumbnails[i].split('"')[-2])
+                temp.append(titles[i].split('"')[-2])
+                temp.append(views[i].split('"')[-2])
+                temp.append(published_time[i].split('"')[-2])
                 report_list.append(temp)
-                
-            # Write the report list to a CSV file
-            csv_file_name = os.path.join(BASE_DIR, search_query+'.csv')
-            with open(csv_file_name, 'w') as csvfile:
-                fieldnames = ['S No', 'Video url', 'Thumbnail', 'Title', 'Views', 'Published Time']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
+            file_name = os.path.join(BASE_DIR, query+'.csv')
+            with open(file_name, 'w') as csvfile:
+                csvwriter = csv.writer(csvfile)
                 for row in report_list:
-                    writer.writerow(row)
-
-            # Insert the report data into the MongoDB collection
-            client = pymongo.MongoClient("mongodb+srv://reddysandeep0904:Sandeep0588@cluster0.x3niyyi.mongodb.net/?retryWrites=true&w=majority")
-            db = client['youtube_scrap']
-            review_col = db['youtube_data']
-            review_col.insert_many(report_list)
-            
-            # Render the result template with the report list and search query
-            return render_template('result.html', report_list=report_list, channel=search_query)
-        
-        # Catch any exceptions and log them to the log file
+                    csvwriter.writerow(row)
+            return render_template('result.html', report_list=report_list, channel=query)
+            # return "Success"
         except Exception as e:
             logging.info(e)
             return render_template('error.html')
-
     else:
         return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
